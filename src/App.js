09d4c46f85480bfc05e854a697159c6df5af019a
Ayush Jain <ayushjain2205@gmail.com>
2023-01-18 03:26:34 +0530

    Add section 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import useWindowDimensions from "./utils/useWindowDimensions";
 import Section1 from "./Components/Section1";
 import Section2 from "./Components/Section2";
+import Section3 from "./Components/Section3";
 
 function App() {
   const { height, width } = useWindowDimensions();
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Section1 height={height} width={width} />
       <Section2 height={height} width={width} />
+      <Section3 height={height} width={width} />
     </div>
   );
 }
---
+++ src/Components/Section3.js
@@ -0,0 +1,22 @@
+import React from "react";
+import phone from "../Assets/phone.png";
+
+const Section3 = ({ height, width }) => {
+  return (
+    <section
+      style={{ height: height, width: width, backgroundColor: "#FEE998" }}
+    >
+      <div className="flex flex-col justify-around h-full">
+        <div className="flex justify-center">
+          <img src={phone} className="h-[499px] w-[261px]" alt="" srcset="" />
+        </div>
+
+        <p className="w-[371px] h-[172px] text-[#EE7D79] text-[36px] font-semibold p-2 text-center">
+          collectibles from your favourite brands
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Section3;
