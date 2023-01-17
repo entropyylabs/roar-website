d0a2c924cc5a3312a0c6b19d917b378fcb215f77
Ayush Jain <ayushjain2205@gmail.com>
2023-01-18 03:34:23 +0530

    Fix section 2 width

--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -11,7 +11,7 @@ const Section2 = ({ height, width }) => {
       }}
       className="relative"
     >
-      <p className="absolute z-50 top-1/3 w-[371px] h-[172px] text-[#1A64BC] text-[36px] font-semibold p-2 text-center">
+      <p className="absolute z-50 top-1/3 w-full h-[172px] text-[#1A64BC] text-[36px] font-semibold p-2 text-center">
         the most fun AR hunt we might have ever played
       </p>
       <img src={section2} alt="" srcset="" className="absolute z-10" />
